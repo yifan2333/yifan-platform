88dc1eafd533af360505a98d3012b86328cd33dd
wuyifan <wuyifan521>
2020-04-28 23:23:24 +0800

    去除session共享配置

--- yifan-gateways/gateway/src/main/java/com/yifan/gateway/GatewayApplication.java
+++ yifan-gateways/gateway/src/main/java/com/yifan/gateway/GatewayApplication.java
@@ -2,20 +2,12 @@ package com.yifan.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
-import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
 
 @SpringBootApplication
-@EnableRedisWebSession
 public class  GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    public WebSessionServerSecurityContextRepository webSessionServerSecurityContextRepository() {
-        return new WebSessionServerSecurityContextRepository ();
-    }
 }
