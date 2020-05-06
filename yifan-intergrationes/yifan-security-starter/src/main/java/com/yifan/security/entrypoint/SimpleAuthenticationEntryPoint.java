850089bbc309c48498073742be94eb7b7edbb451
wuyifan <wuyifan521>
2020-05-06 10:33:15 +0800

    管理poi版本号

--- yifan-intergrationes/yifan-security-starter/src/main/java/com/yifan/security/entrypoint/SimpleAuthenticationEntryPoint.java
+++ yifan-intergrationes/yifan-security-starter/src/main/java/com/yifan/security/entrypoint/SimpleAuthenticationEntryPoint.java
@@ -17,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 
 /** 
  * AuthenticationException 是在用户认证的时候出现错误时抛出的异常
- * 系统用户不存在，被锁定，凭证失效，密码错误等认证过程中出现的异常都由 AuthenticationException 处理。 401
+  * 系统用户不存在，被锁定，凭证失效，密码错误等认证过程中出现的异常都由 AuthenticationException 处理。 401
  *
  * @author: wuyifan
  * @since: 2019年12月10日 下午3:11
