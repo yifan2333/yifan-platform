2955021ce894a1ce8f2f69ee064235e49ba78185
wuyifan <wuyifan521>
2020-05-15 11:09:23 +0800

    添加两台测试实例

---
+++ yifan-servers/server-b/src/main/java/com/yifan/server/ServerAApplication.java
@@ -0,0 +1,13 @@
+package com.yifan.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerAApplication.class, args);
+    }
+
+}
---
+++ yifan-servers/server-b/src/main/java/com/yifan/server/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.yifan.server.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test1(HttpServletRequest request) throws InterruptedException {
+        Map<String, String[]> map = request.getParameterMap();
+
+        System.out.println(JSONObject.toJSONString(map));
+
+        Thread.sleep(10);
+        return "test";
+    }
+
+}
